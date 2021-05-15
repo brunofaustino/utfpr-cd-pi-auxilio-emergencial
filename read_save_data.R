4cc1ae6463096abb7093df320f52f470b8cc9828
brunofaustino <b.fa.amorim@gmail.com>
2021-05-15 10:14:25 -0300

    Update read_save_data.R
    Atualização da modelagem

--- read_save_data.R
+++ read_save_data.R
@@ -1,7 +1,12 @@
+#install.packages("tidyverse")
+#install.packages("DBI")
+#install.packages("RPostgres")
+
 library(tidyverse)
 library(DBI)
+library(RPostgres)
 
-df <- read_csv2('./auxilio_emergencial.csv', col_names = c('ano_disponibilizacao', 'mes_disponibilizacao',
+df <- read_csv2('C:\\Users\\bfaam\\Downloads\\auxilio_emergencial.csv', col_names = c('ano_disponibilizacao', 'mes_disponibilizacao',
                                                            'uf', 'nome_municipio',
                                                            'parcela_1', 'parcela_2',
                                                            'parcela_3', 'parcela_4',
@@ -29,30 +34,29 @@ df <- read_csv2('./auxilio_emergencial.csv', col_names = c('ano_disponibilizacao
                                                                col_integer(), col_integer(),
                                                                col_integer(), col_integer(),
                                                                col_integer()))
-
+df
 df[df == 'NULL'] <- '0'
 df[is.na(df)] <- 0
 
 df <- tibble::rowid_to_column(df, "id")
 
-municipio <- df[, c(4, 5)] %>% distinct(nome_municipio, .keep_all = TRUE)
+municipio <- df[, c(4, 5)] %>% distinct(uf, nome_municipio, .keep_all = TRUE)
 
-parcela <- df[, c(1,5,2,3,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23)] %>% mutate(id_parcela_extra = id)
+parcela <- df[, c(1,4,5,2,3,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23)]
 
 parcela[, c(14:23)] <- lapply(parcela[, c(14:23)], as.numeric)
 
-parcela_extra <- df[, c(1,24,25,26,27,28)]
+parcela_extra <- df[, c(1,4,5,24,25,26,27,28)]
 
 names(municipio) <- tolower(names(municipio))
 names(parcela_extra) <- tolower(names(parcela_extra))
 names(parcela) <- tolower(names(parcela))
 
-
-con <- dbConnect(RPostgres::Postgres(),dbname = 'auxilio_emergencial',   options="-c search_path=auxilio",
+con <- dbConnect(RPostgres::Postgres(),dbname = 'postgres',   options="-c search_path=auxilio",
                  host = 'localhost', 
                  port = 5432, 
                  user = 'postgres',
-                 password = 'postgres123')
+                 password = 'admin')
 
 
 dbAppendTable(con, "municipio", municipio)
@@ -60,5 +64,3 @@ dbAppendTable(con, "parcela_extra", parcela_extra)
 dbAppendTable(con, "parcela", parcela)
 
 dbDisconnect(con)
-
-
